c14c78b172b8cc69bd2950cffde3d0e590cc4ff2
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-05-20 12:15:15 +0200

    Add unittests

---
+++ tests/test_cpp.py
@@ -0,0 +1,13 @@
+import unittest
+import subprocess
+import os
+
+class MainTest(unittest.TestCase):
+    def test_cpp(self):
+        print("\n\nTesting C++ code")
+        subprocess.check_call(os.path.join(os.path.dirname(
+            os.path.relpath(__file__)), "bin", "python_cpp_example_test"))
+        print()
+
+if __name__ == "__main__":
+    unittest.main()
