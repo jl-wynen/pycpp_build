9fbf9129f9bb0f72a43cde1020000d3eb61e5071
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-05-22 13:47:14 +0200

    Separate config / compile steps

--- setup.py
+++ setup.py
@@ -4,71 +4,147 @@ import sys
 import sysconfig
 import platform
 import subprocess
+from pathlib import Path
+import pickle
 
 from shutil import copyfile, copymode
 from distutils.version import LooseVersion
+from distutils.cmd import Command
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from setuptools.command.test import test as TestCommand
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+BUILD_DIR = PROJECT_ROOT/"build"
+CONFIG_FILE = BUILD_DIR/"configure.out.pkl"
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
+def parse_shell_command(cmd):
+    if not cmd:
+        return None
 
-class CMakeBuild(build_ext):
+    status, output = subprocess.getstatusoutput(f"which {cmd}")
+    if status == 0:
+        return cmd
+    print(f"Error: cannot execute command {cmd}:")
+    print(f"  {output}")
+    sys.exit(1)
+
+def parse_file(fname):
+    if not fname:
+        return None
+
+    path = Path(fname)
+    if path.exists():
+        return path.resolve()
+    print(f"Error: path '{path}' does not exist")
+    sys.exit(1)
+
+
+class ConfigureCommand(Command):
+    description = "Configure C++ extension"
+    user_options = [
+        ("compiler=", None, "C++ compiler to use"),
+        ("catch=", None, "path to catch"),
+        ("build-type=", None, "CMake build type"),
+    ]
+
+    def initialize_options(self):
+        """Set default values"""
+        self.compiler = ""  # use default compiler
+        self.catch = ""  # search in system path
+        self.build_type = "devel"
+
+    def finalize_options(self):
+        """Post-process options"""
+
+        self.compiler = parse_shell_command(self.compiler)
+        self.catch = parse_file(self.catch)
+        self.build_type = self.build_type.upper()
+        if self.build_type not in ("DEVEL", "DEBUG", "RELEASE"):
+            print(f"Error: build type not supported: {self.build_type}")
+            sys.exit(1)
+
     def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
+        BUILD_DIR.mkdir(exist_ok=True)
+        pickle.dump({"CMAKE_CXX_COMPILER": self.compiler,
+                     "CMAKE_BUILD_TYPE": self.build_type,
+                     "CATCH_INCLUDE": self.catch,
+                    },
+                    open(str(CONFIG_FILE), "wb"))
 
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                         out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
+def common_cmake_args(config):
+    return [f"-D{key}={val}" for key, val in config.items() if val] \
+        + [f"-DPYTHON_EXECUTABLE={sys.executable}",
+           f"-DPYTHON_CONFIG={sys.executable}-config"]
+
+
+class BuildExtension(build_ext):
+    def run(self):
+        # make sure that cmake is installed
+        _ = parse_shell_command("cmake")
+
+        # TODO message if not configured
+        config = pickle.load(open(str(CONFIG_FILE), "rb"))
+        config_time = CONFIG_FILE.stat().st_mtime
+        cmake_args = common_cmake_args(config)
+        
         for ext in self.extensions:
-            self.build_extension(ext)
+            self._build_extension(ext, cmake_args, config_time)
 
-    def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        sys.exit(0)
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+    def _run_cmake(self, extension, ext_build_dir, cmake_args):
+        print("running cmake")
 
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
+        # where the extension library has to be placed (during build, not installation)
+        extdir = Path(self.get_ext_fullpath(extension.name)).parent.resolve()
+        # finalize arguments for cmake
+        cmake_args = cmake_args \
+                     + [f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}"]
 
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args,
-                              cwd=self.build_temp)
-        # Copy *_test file to tests directory
-        test_bin = os.path.join(self.build_temp, 'python_cpp_example_test')
-        self.copy_test_file(test_bin)
-        print()  # Add an empty line for cleaner output
+        try:
+            # call cmake
+            subprocess.check_call(['cmake', extension.sourcedir] + cmake_args,
+                                  cwd=ext_build_dir)
+        except subprocess.CalledProcessError as err:
+            print(f"Calling cmake failed with arguments {err.cmd}")
+            sys.exit(1)
+
+    def _build_extension(self, extension, cmake_args, config_time):
+        print(f"building extension {extension.name}")
+
+        ext_build_dir = Path(self.build_temp).resolve()/(extension.name.rsplit("/", 1)[1])
+
+        if ext_build_dir.exists() and ext_build_dir.stat().st_mtime < config_time:
+            ext_build_dir.rmdir()
+
+        if not ext_build_dir.exists():
+            ext_build_dir.mkdir(parents=True)
+            self._run_cmake(extension, ext_build_dir, cmake_args)
+
+        print("compiling extension")
+        subprocess.check_call(['cmake', '--build', '.'],
+                              cwd=ext_build_dir)
+
+        # env = os.environ.copy()
+        # env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+        #     env.get('CXXFLAGS', ''),
+        #     self.distribution.get_version())
+        # if not os.path.exists(self.build_temp):
+        #     os.makedirs(self.build_temp)
+        # subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+        #                       cwd=self.build_temp, env=env)
+
+        # # Copy *_test file to tests directory
+        # test_bin = os.path.join(self.build_temp, 'python_cpp_example_test')
+        # self.copy_test_file(test_bin)
+        # print()  # Add an empty line for cleaner output
 
     def copy_test_file(self, src_file):
         '''
@@ -90,25 +166,25 @@ class CMakeBuild(build_ext):
         copyfile(src_file, dest_file)
         copymode(src_file, dest_file)
         
-class CatchTestCommand(TestCommand):
-    "Execute Python and C++ Catch tests"
+# class CatchTestCommand(TestCommand):
+#     "Execute Python and C++ Catch tests"
 
-    def distutils_dir_name(self, dname):
-        """Returns the name of a distutils build directory"""
-        dir_name = "{dirname}.{platform}-{version[0]}.{version[1]}"
-        return dir_name.format(dirname=dname,
-                               platform=sysconfig.get_platform(),
-                               version=sys.version_info)
+#     def distutils_dir_name(self, dname):
+#         """Returns the name of a distutils build directory"""
+#         dir_name = "{dirname}.{platform}-{version[0]}.{version[1]}"
+#         return dir_name.format(dirname=dname,
+#                                platform=sysconfig.get_platform(),
+#                                version=sys.version_info)
 
-    def run(self):
-        # Run catch tests
-        subprocess.call(['./*_test'],
-                        cwd=os.path.join('build',
-                                         self.distutils_dir_name('temp')),
-                        shell=True)
-        # Run Python tests
-        super(CatchTestCommand, self).run()
-        print("\nPython tests complete, now running C++ tests...\n")
+#     def run(self):
+#         # Run catch tests
+#         subprocess.call(['./*_test'],
+#                         cwd=os.path.join('build',
+#                                          self.distutils_dir_name('temp')),
+#                         shell=True)
+#         # Run Python tests
+#         super(CatchTestCommand, self).run()
+#         print("\nPython tests complete, now running C++ tests...\n")
 
 setup(
     name='pycpp_build',
@@ -122,15 +198,17 @@ setup(
     # tell setuptools that all packages will be under the 'src' directory
     # and nowhere else
     package_dir={'':'src'},
-    # add an extension module named 'python_cpp_example' to the package
-    # 'python_cpp_example'
+    # add an extension module named 'pycpp_build' to the package
+    # 'pycpp_build'
     ext_modules=[CMakeExtension('pycpp_build/pycpp_build')],
-    # add custom build_ext command
-    cmdclass=dict(build_ext=CMakeBuild),
+    cmdclass={
+        "configure": ConfigureCommand,
+        "build_ext": BuildExtension,
+    },
     zip_safe=False,
     python_requires=">=3.6.5",
     entry_points={
         "console_scripts": ["hei = pycpp_build.main:main"]
-    }
+    },
+    install_requires=["numpy>=1.14.3"],
 )
-
