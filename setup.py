756328eee218f191108903043c3fdfd4a0cd4102
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-05-24 13:51:29 +0200

    Add version_from_git

--- setup.py
+++ setup.py
@@ -219,9 +219,31 @@ class BuildExtension(build_ext):
             print(f"Calling cmake to build failed, arguments {err.cmd}")
             sys.exit(1)
 
+def version_from_git(plain=True):
+    """
+    Get a version number from the latest git tag.
+    Tags have to contain a version number of the form
+    v0.1, v0.1.3, etc. where the leading v is optional.
+    The number may be followed by arbitrary text.
+
+    If plain=True, only the version number is returned, otherwise anything following
+    the number is included as well. The v is always omitted.
+    """
+
+    try:
+        latest_tag = subprocess.check_output(["git", "describe", "--tag"]).decode("utf-8")
+        match = re.match(r"v?((\d[\d\.]*).*)\n?", str(latest_tag))
+        if match is None:
+            print(f"error: latest tag does not conform to format of version number: {latest_tag}")
+        if plain:
+            return match[2]
+        return match[1]
+    except subprocess.CalledProcessError:
+        return "0.0"
+
 setup(
     name="pycpp_build",
-    version="0.1",
+    version=version_from_git(plain=True),
     author="Jan-Lukas Wynen",
     author_email="j-l.wynen@hotmail.de",
     description="A hybrid Python/C++ test project",
