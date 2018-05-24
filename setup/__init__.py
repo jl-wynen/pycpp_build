a561d8b2f5e4113850310407f415952ca7c2ae9a
Jan-Lukas Wynen <j-l.wynen@hotmail.de>
2018-05-24 14:30:09 +0200

    Add init for setup package

---
+++ setup/__init__.py
@@ -0,0 +1,5 @@
+from .configure import configure_command
+from .cmake_extension import CMakeExtension
+from .build import get_cmake_builder
+from .version import version_from_git
+from . import predicate
